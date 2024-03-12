f58522caefc9bfd120dc2722ae63671e8ca33c09
James Bradlee <james.bradlee@telenor.no>
2024-03-12 17:17:40 +0100

    feat: support for log fields

--- logpayload.go
+++ logpayload.go
@@ -14,22 +14,24 @@ type LogPayload struct {
 	level  LogLevel
 	format string
 	args   []any
+	fields map[string]string
 
 	// Information about the caller
 
 	callerInfo *caller.Info
 }
 
-func (p *LogPayload) Name() string        { return p.name }
-func (p *LogPayload) Level() LogLevel     { return p.level }
-func (p *LogPayload) Format() string      { return p.format }
-func (p *LogPayload) Args() []any         { return p.args }
-func (p *LogPayload) PackageName() string { return p.callerInfo.Sig.Package }
-func (p *LogPayload) HasReceiver() bool   { return p.callerInfo.Sig.HasReceiver }
-func (p *LogPayload) Receiver() string    { return p.callerInfo.Sig.Receiver }
-func (p *LogPayload) Function() string    { return p.callerInfo.Sig.Function }
-func (p *LogPayload) File() string        { return p.callerInfo.File }
-func (p *LogPayload) Line() int           { return p.callerInfo.Line }
+func (p *LogPayload) Name() string              { return p.name }
+func (p *LogPayload) Level() LogLevel           { return p.level }
+func (p *LogPayload) Format() string            { return p.format }
+func (p *LogPayload) Args() []any               { return p.args }
+func (p *LogPayload) Fields() map[string]string { return p.fields }
+func (p *LogPayload) PackageName() string       { return p.callerInfo.Sig.Package }
+func (p *LogPayload) HasReceiver() bool         { return p.callerInfo.Sig.HasReceiver }
+func (p *LogPayload) Receiver() string          { return p.callerInfo.Sig.Receiver }
+func (p *LogPayload) Function() string          { return p.callerInfo.Sig.Function }
+func (p *LogPayload) File() string              { return p.callerInfo.File }
+func (p *LogPayload) Line() int                 { return p.callerInfo.Line }
 
 func newLogPayload(
 	name string,
@@ -42,6 +44,7 @@ func newLogPayload(
 		level:      level,
 		format:     format,
 		args:       args,
+		fields:     configuredDriver.MdcCopy(),
 		callerInfo: caller.Get(2),
 	}
 	return p
